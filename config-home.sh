0c0e55112c5120930149661b6db86e968a505b30
Joshua L. Phillips <Joshua.Phillips@mtsu.edu>
2025-10-20 08:16:12 -0500

    Additional code-server extensions, fix to config scripts

--- config-home.sh
+++ config-home.sh
@@ -25,13 +25,16 @@ if [ ! -f ${HOME}/.bashrc ]; then
    echo "Adding .bashrc"
    if [ "$(id -u)" == 0 ]; then
       su -c "cp /etc/skel/.bashrc ${HOME}/." ${NB_USER}
-      su -c "mamba shell init" ${NB_USER}
-      su -c "conda init" ${NB_USER}
+      su -c "/opt/conda/bin/mamba shell init" ${NB_USER}
+      su -c "/opt/conda/bin/conda init" ${NB_USER}
    else
       cp /etc/skel/.bashrc ${HOME}/.
-      mamba shell init
-      conda init
+      /opt/conda/bin/mamba shell init
+      /opt/conda/bin/conda init
    fi
+   echo -e "export NVM_DIR=\"/opt/nvm\"" >> ${HOME}/.bashrc
+   echo -e "[ -s \"$NVM_DIR/nvm.sh\" ] && \\. \"$NVM_DIR/nvm.sh\"  # This loads nvm" >> ${HOME}/.bashrc
+   echo -e "[ -s \"$NVM_DIR/bash_completion\" ] && \\. \"$NVM_DIR/bash_completion\"  # This loads nvm bash_completion" >> ${HOME}/.bashrc
 fi
 
 echo "Finished hook to prepare home directory: ${HOME}"
